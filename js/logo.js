0de4a122cb31c00ea6cce44bbb9248dac53a5480
Ishan <ishan28mkip@gmail.com>
2015-07-31 19:20:04 -0700

    Minor AddChild  -> add edit in activity.js

--- js/logo.js
+++ js/logo.js
@@ -1,13 +1,3 @@
-// Copyright (c) 2014,2015 Walter Bender
-//
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 3 of the License, or
-// (at your option) any later version.
-//
-// You should have received a copy of the GNU General Public License
-// along with this library; if not, write to the Free Software
-// Foundation, 51 Franklin Street, Suite 500 Boston, MA 02110-1335 USA
 
 var DEFAULTBACKGROUNDCOLOR = [70, 80, 20];
 var DEFAULTDELAY = 500; // milleseconds
