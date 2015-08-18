3363d0621dbeb4e0639c1f98ed16ae7c9d5f076b
Ishan <ishan28mkip@gmail.com>
2015-08-19 03:33:04 +0530

    Added scaling in the trash bitmaps

--- js/trash.js
+++ js/trash.js
@@ -28,44 +28,86 @@ function Trashcan (canvas, stage, size, refreshCanvas) {
     this.refreshCanvas = refreshCanvas;
     this.size = size;
     this.iconsize = 55;  // default value
+    
+    this.icon  = null;
+    this.border = null;
+    this.borderHighlight = null;
+
     this.container = new THREE.Group();
 
     function makeBorderHighlight(me) {
+        // FIXME : Scale
+        var scale = 1;
+        scale = me.size / me.iconsize;
 
         var img = new Image();
-        img.onload = function(){
-            var texture = new THREE.Texture(img) 
+        img.onload = function () {
+
+            var canvas = document.createElement('canvas');
+            canvas.width = img.width * scale;
+            canvas.height = img.height * scale;
+            var context = canvas.getContext('2d');
+            context.drawImage(img, 0, 0, img.width * scale, img.height * scale);
+            var texture = new THREE.Texture(canvas);
             texture.needsUpdate = true;
-            var material = new THREE.MeshBasicMaterial( {map: texture, side: THREE.DoubleSide} );
+            texture.minFilter = THREE.NearestFilter; 
+            var material = new THREE.MeshBasicMaterial( {map: texture} );
             material.transparent = true;
-            var border = new THREE.Mesh(new THREE.PlaneBufferGeometry(img.width*me.container.scaleX, img.height*me.container.scaleY),material);
-            me.container.add(border);
-            border.name = 'trashBorderHighlight';
-            // bounds = me.container.getBounds(); //cache feature does not make sense for webgl based rendering
-            // me.container.cache(bounds.x, bounds.y, bounds.width, bounds.height);
-            // Hide the trash until a block is moved
+            material.depthWrite = false;
+
+            var borderHighlight = new THREE.Mesh(new THREE.PlaneBufferGeometry(img.width * scale, img.height * scale),material);
+            borderHighlight.name = 'trashBorderHighlight';
+            borderHighlight.imgWidth = img.width * scale;
+            borderHighlight.imgHeight = img.height * scale;
+            borderHighlight.initialWidth = img.width;
+            borderHighlight.initialHeight = img.height;
+
+            me.borderHighlight = borderHighlight;
+
+            me.container.add(borderHighlight);
             me.container.visible = false;
-            refreshCanvas(1);
+            me.borderHighlight.visible = false;
+            me.icon.visible = true;
+            me.border.visible = true;
+            
         }
         img.src = 'data:image/svg+xml;base64,' + window.btoa(
             unescape(encodeURIComponent(BORDER.replace('stroke_color', '#555555'))));
     }
 
     function makeBorder(me) {
+        var scale = 1;
+        scale = me.size / me.iconsize;
+
         var img = new Image();
-        img.onload = function(){
-            var texture = new THREE.Texture(img) 
+        img.onload = function () {
+
+            var canvas = document.createElement('canvas');
+            canvas.width = img.width * scale;
+            canvas.height = img.height * scale;
+            var context = canvas.getContext('2d');
+            context.drawImage(img, 0, 0, img.width * scale, img.height * scale);
+            var texture = new THREE.Texture(canvas);
             texture.needsUpdate = true;
-            var material = new THREE.MeshBasicMaterial( {map: texture, side: THREE.DoubleSide} );
+            texture.minFilter = THREE.NearestFilter; 
+            var material = new THREE.MeshBasicMaterial( {map: texture} );
             material.transparent = true;
-            var border = new THREE.Mesh(new THREE.PlaneBufferGeometry(img.width*me.container.scaleX, img.height*me.container.scaleY),material);
-            me.container.add(border);
+            material.depthWrite = false;
+
+            var border = new THREE.Mesh(new THREE.PlaneBufferGeometry(img.width * scale, img.height * scale),material);
             border.name = 'trashBorder';
-            
+            border.imgWidth = img.width * scale;
+            border.imgHeight = img.height * scale;
+            border.initialWidth = img.width;
+            border.initialHeight = img.height;
+
+            me.border = border;
+
+            me.container.add(border);
+
             me.container.hitmesh = border;
             border.parentMesh = me.container;
 
-            // Remove these when done
             me.container.on('mouseover',function(event){
                 me.highlight();
             });
@@ -73,37 +115,60 @@ function Trashcan (canvas, stage, size, refreshCanvas) {
                 me.unhighlight();
             });
 
+            border.visible = false;
+
             makeBorderHighlight(me);
+            
         }
         img.src = 'data:image/svg+xml;base64,' + window.btoa(
             unescape(encodeURIComponent(BORDER.replace('stroke_color', '#e0e0e0'))));
     }
 
     function makeTrash(me) {
+        var scale = 1;
+        scale = me.size / me.iconsize;
+
         var img = new Image();
         img.onload = function () {
-            var texture = new THREE.Texture(img) 
+
+            var canvas = document.createElement('canvas');
+            canvas.width = img.width * scale;
+            canvas.height = img.height * scale;
+            var context = canvas.getContext('2d');
+            context.drawImage(img, 0, 0, img.width * scale, img.height * scale);
+            var texture = new THREE.Texture(canvas);
             texture.needsUpdate = true;
-            var material = new THREE.MeshBasicMaterial( {map: texture, side: THREE.DoubleSide} );
+            texture.minFilter = THREE.NearestFilter; 
+            var material = new THREE.MeshBasicMaterial( {map: texture} );
             material.transparent = true;
-            me.container.scaleX = size/me.iconsize;
-            me.container.scaleY = size/me.iconsize;
-            var bitmap = new THREE.Mesh(new THREE.PlaneBufferGeometry(me.container.scaleX*img.width, me.container.scaleY*img.height),material);
-            me.container.add(bitmap);
-            bitmap.name = 'trashIcon';
-            // var axes = buildAxes( 1000 );
-            // me.container.add( axes );
-            // me.iconsize = bitmap.getBounds().width; //PE : see why is this done
-            me.container.position.set(0, -window.innerHeight/2 + ((TRASHHEIGHT + size) / 2) * me.container.scaleY ,1);
-            makeBorder(me);
+            material.depthWrite = false;
+
+            var trashIcon = new THREE.Mesh(new THREE.PlaneBufferGeometry(img.width * scale, img.height * scale),material);
+            trashIcon.name = 'trashIcon';
+            trashIcon.imgWidth = img.width * scale;
+            trashIcon.imgHeight = img.height * scale;
+            trashIcon.initialWidth = img.width;
+            trashIcon.initialHeight = img.height;
+
+            me.icon = trashIcon;
+            me.container.add(me.icon);
+            me.icon.visible = false;
+
+            me.container.position.setX(0);
+            me.container.position.setY(-window.innerHeight/2 + ((TRASHHEIGHT + size) / 2));
+            // FIXME : Scaling
+            // me.container.position.setY(-window.innerHeight/2 + ((TRASHHEIGHT + size) / 2) * me.container.scaleY);
+
+            makeBorder(me);            
         }
         img.src = 'images/trash.svg';
     }
 
-    // TODO set the y properly
     this.resizeEvent = function(scale) {
         this.container.position.setX(0);
-        this.container.position.setY(-window.innerHeight/2 + ((TRASHHEIGHT + size) / 2) * this.container.scaleY);
+        this.container.position.setY(-window.innerHeight/2 + ((TRASHHEIGHT + size) / 2));
+        // FIXME : Scaling
+        // this.container.position.setY(-window.innerHeight/2 + ((TRASHHEIGHT + size) / 2) * this.container.scaleY);
     }
 
     this.stage.add(this.container);
